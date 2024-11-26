6e96c73f7672f2bd6a38e59e6eea4957d4dcd6ef
Joshua Davis <joshuad100717@outlook.com>
2024-11-26 10:44:58 -0700

    contactly

---
+++ API/Contactly/Contactly/Controllers/ContactsController.cs
@@ -0,0 +1,43 @@
+﻿using Contactly.Data;
+using Contactly.Models;
+using Contactly.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contactly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactlyDbContext dbContext;
+        public ContactsController(ContactlyDbContext dbContext)
+        {
+            this.dbContext = dbContext; 
+        }
+
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+            var contacts = dbContext.Contacts.ToList();
+            return Ok(contacts);
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(AddContactRequestDto request)
+        {
+            var domainModelContact = new Contact
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                Favorite = request.Favorite
+            };
+
+            dbContext.Contacts.Add(domainModelContact);
+            dbContext.SaveChanges();
+            return Ok(domainModelContact);
+        }
+    }
+}
